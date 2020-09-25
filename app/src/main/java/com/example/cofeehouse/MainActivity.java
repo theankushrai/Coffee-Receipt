109aedced7d2fd19c495a77756b8672be5e14534
theankushrai <iamankushrai@gmail.com>
2020-09-25 21:33:17 +0530

    Checkboxes and name field added.(information about checkboxes to be shown in order information when checkboxes are clicked and button only allowed to be clicked when some name is written in edittext)

--- app/src/main/java/com/example/cofeehouse/MainActivity.java
+++ app/src/main/java/com/example/cofeehouse/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,31 +18,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     int noOfCofee = 0;
+    int totalPrice = 0;
+    boolean hasWhippedCream = false;
+    boolean hasChocolate = false;
 
     public void increment(View view) {
         noOfCofee = noOfCofee + 1;
-        displayQuantityPrice();
+        displayQuantity();
     }
 
     public void decrement(View view) {
         if (noOfCofee != 0) {
             noOfCofee = noOfCofee - 1;
         }
-        displayQuantityPrice();
+        displayQuantity();
     }
 
-    public void displayQuantityPrice() {
+    public void displayQuantity() {
         TextView quantity = findViewById(R.id.quantity);
-        TextView price = findViewById(R.id.price);
         quantity.setText(noOfCofee + "");
-        price.setText("$ " + (noOfCofee * 4));
     }
+
+    public void expandCheckBoxes(View view) {
+        CheckBox whippedCream = findViewById(R.id.whippedCream);
+        CheckBox chocolate = findViewById(R.id.chocolate);
+        whippedCream.setVisibility(View.VISIBLE);
+        chocolate.setVisibility(View.VISIBLE);
+    }
+
+    public void addedExtraWhippedCream(View view) {
+        hasWhippedCream = true;
+    }
+
+    public void addedExtraChocolate(View view) {
+        hasChocolate = true;
+    }
+
 
     public void orderButtonClick(View view) {
         TextView orderSummary = findViewById(R.id.orderSummary);
         TextView orderSummaryHeading = findViewById(R.id.orderSummaryHeading);
+        EditText editName = findViewById(R.id.editName);
         orderSummary.setVisibility(View.VISIBLE);
         orderSummaryHeading.setVisibility(View.VISIBLE);
-        orderSummary.setText("Thank you!!!\nHere's your bill\nTotal price : $ " + (noOfCofee * 4) + "\nCome Again");
+        totalPrice = noOfCofee * 4;
+        orderSummary.setText("Thank you" + editName.getText().toString() + "\nTotal : $" + totalPrice);
+        Toast.makeText(this, "Ordered!!", Toast.LENGTH_SHORT).show();
     }
 }
