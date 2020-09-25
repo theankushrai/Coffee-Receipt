4f0d089058c7789fe8f0aa8427cc46a0602e90cd
theankushrai <iamankushrai@gmail.com>
2020-09-25 16:44:01 +0530

    Functionalities added activity main file

--- app/src/main/java/com/example/cofeehouse/MainActivity.java
+++ app/src/main/java/com/example/cofeehouse/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cofeehouse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    int noOfCofee = 0;
+
+    public void increment(View view) {
+        noOfCofee = noOfCofee + 1;
+        displayQuantityPrice();
+    }
+
+    public void decrement(View view) {
+        if (noOfCofee != 0) {
+            noOfCofee = noOfCofee - 1;
+        }
+        displayQuantityPrice();
+    }
+
+    public void displayQuantityPrice() {
+        TextView quantity = findViewById(R.id.quantity);
+        TextView price = findViewById(R.id.price);
+        quantity.setText(noOfCofee + "");
+        price.setText("$ " + (noOfCofee * 4));
+    }
+
+    public void orderButtonClick(View view) {
+        TextView orderSummary = findViewById(R.id.orderSummary);
+        TextView orderSummaryHeading = findViewById(R.id.orderSummaryHeading);
+        orderSummary.setVisibility(View.VISIBLE);
+        orderSummaryHeading.setVisibility(View.VISIBLE);
+        orderSummary.setText("Thank you!!!\nHere's your bill\nTotal price : $ " + (noOfCofee * 4) + "\nCome Again");
     }
 }
